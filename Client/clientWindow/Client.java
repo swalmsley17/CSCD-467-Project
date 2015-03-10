6dc4492382581bbd011aac38b38897a415531161
ewu-phillipsk <kphillips2010@eagles.ewu.edu>
2015-03-10 16:13:31 -0700

    Updated Client Window
    Most everything is in place except for connecting and sending to server.
    Need to add place for user to enter ip + port. Can currently open and
    edit files and save them. Client in place to begin sending files within
    a day or so.
    
    Code is a little bit messy, will be cleaned up later!

---
+++ Client/clientWindow/Client.java
@@ -0,0 +1,16 @@
+package clientWindow;
+
+public class Client {
+	
+	public Client(){
+		
+	}
+	
+	public boolean connect(){
+		return false;
+	}
+	
+	public void sendData(){
+		
+	}
+}
--- Client/clientWindow/ClientWindow.java
+++ Client/clientWindow/ClientWindow.java
@@ -6,24 +6,48 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.JMenuBar;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+
 import javax.swing.JMenuItem;
 import javax.swing.JMenu;
 import javax.swing.JSeparator;
+import javax.swing.JEditorPane;
+import javax.swing.JTextArea;
+import javax.swing.BorderFactory;
+
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.io.*;
+import java.util.ArrayList;
+
+import javax.swing.JFileChooser;
+
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
+
+import javax.swing.ScrollPaneConstants;
 
 public class ClientWindow extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField insertCodeTextField;
-	private JTextField serverMessagesTextfield;
+	private ArrayList<File> sourceFiles = new ArrayList<File>();
+	private String textPreEdit = "";
+	private String textPostEdit = "";
+	private File selectedFile;
+	private Client client;
 
 	/**
 	 * Launch the application.
@@ -41,82 +65,193 @@ public class ClientWindow extends JFrame {
 		});
 	}
 
+	/***********************************************************************************************************************/
+
 	/**
 	 * Create the frame.
 	 */
 	public ClientWindow() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 620, 370);
-		
+
+		// MENU STUFF
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
-		
+
 		JMenu mnFile = new JMenu("File");
 		menuBar.add(mnFile);
-		
+
 		JMenuItem mntmExit = new JMenuItem("Exit");
+		mntmExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
 		mnFile.add(mntmExit);
-		
-		JMenu mnEdit = new JMenu("Edit");
-		menuBar.add(mnEdit);
-		
+
 		JMenu mnHelp = new JMenu("Help");
 		menuBar.add(mnHelp);
-		
+
 		JMenuItem mntmAbout = new JMenuItem("About");
+		mntmAbout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JOptionPane.showMessageDialog(null, "About");
+			}
+		});
 		mnHelp.add(mntmAbout);
-		
+
+		// MAIN CONTENT PANEL
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JLabel javaFilesLabel = new JLabel("Java Files:");
-		javaFilesLabel.setBounds(10, 11, 53, 14);
-		contentPane.add(javaFilesLabel);
-		
-		JLabel insertCodeHereLabel = new JLabel("Insert Code Here:");
-		insertCodeHereLabel.setBounds(191, 11, 96, 14);
+
+		// INSERT CODE TEXT AREA + SCROLL PANE
+		JTextArea insertCodeTextArea = new JTextArea();
+		insertCodeTextArea.setTabSize(3);
+		insertCodeTextArea.setBounds(111, 41, 403, 259);
+		insertCodeTextArea.setBorder(BorderFactory
+				.createLineBorder(Color.black));
+		contentPane.add(insertCodeTextArea);
+
+		JScrollPane codeScrollPane = new JScrollPane(insertCodeTextArea);
+		codeScrollPane.setBounds(191, 40, 403, 259);
+		contentPane.add(codeScrollPane);
+
+		JLabel insertCodeHereLabel = new JLabel("Your Code Goes Here:");
+		insertCodeHereLabel.setBounds(191, 15, 132, 14);
 		contentPane.add(insertCodeHereLabel);
-		
-		insertCodeTextField = new JTextField();
-		insertCodeTextField.setBounds(191, 36, 403, 263);
-		contentPane.add(insertCodeTextField);
-		insertCodeTextField.setColumns(10);
-		
-		JButton submitCodeButton = new JButton("Submit Question");
-		submitCodeButton.setBounds(10, 169, 132, 37);
-		contentPane.add(submitCodeButton);
-		
-		JLabel serverMessagesLabel = new JLabel("Server Messages:");
-		serverMessagesLabel.setBounds(10, 217, 86, 14);
-		contentPane.add(serverMessagesLabel);
-		
-		serverMessagesTextfield = new JTextField();
-		serverMessagesTextfield.setBounds(10, 242, 171, 57);
-		contentPane.add(serverMessagesTextfield);
-		serverMessagesTextfield.setColumns(10);
-		
-		JComboBox javaFilesComboBox = new JComboBox();
-		javaFilesComboBox.setBounds(10, 36, 132, 20);
-		contentPane.add(javaFilesComboBox);
-		
-		JButton nextButton = new JButton("Next Question");
-		nextButton.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				JOptionPane.showMessageDialog(null, "Hello World");
+
+		// SAVE AND SUBMIT BUTTON
+		JButton submitCodeButton = new JButton("Save & Submit File");
+		submitCodeButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				saveFile();
+				sendToServer();
 			}
 		});
-		nextButton.setBounds(10, 67, 132, 23);
-		contentPane.add(nextButton);
-		
-		JButton previousQuestionButton = new JButton("Previous Question");
-		previousQuestionButton.setBounds(10, 101, 132, 23);
-		contentPane.add(previousQuestionButton);
-		
-		JButton skipButton = new JButton("Skip Question");
-		skipButton.setBounds(10, 135, 132, 23);
-		contentPane.add(skipButton);
+		submitCodeButton.setBounds(10, 147, 171, 37);
+		contentPane.add(submitCodeButton);
+
+		JLabel serverMessagesLabel = new JLabel("Server Messages:");
+		serverMessagesLabel.setBounds(10, 195, 171, 14);
+		contentPane.add(serverMessagesLabel);
+
+		// SERVER MESSAGE TEXT AREA
+		JTextArea serverMessagesTextArea = new JTextArea();
+		serverMessagesTextArea.setTabSize(4);
+		serverMessagesTextArea.setEditable(false);
+		serverMessagesTextArea.setBounds(10, 220, 171, 79);
+		serverMessagesTextArea.setBorder(BorderFactory
+				.createLineBorder(Color.black));
+		contentPane.add(serverMessagesTextArea);
+
+		// OPENED JAVA FILE TEXT AREA + SCROLL PANE
+		JTextArea javaFileOpenedTextArea = new JTextArea();
+		javaFileOpenedTextArea.setBounds(10, 80, 169, 35);
+		contentPane.add(javaFileOpenedTextArea);
+		javaFileOpenedTextArea.setEditable(false);
+		javaFileOpenedTextArea.setBorder(BorderFactory
+				.createLineBorder(Color.black));
+
+		JScrollPane selectFileScrollPane = new JScrollPane(
+				javaFileOpenedTextArea);
+		selectFileScrollPane
+				.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		selectFileScrollPane
+				.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+		selectFileScrollPane.setBounds(10, 58, 171, 44);
+		contentPane.add(selectFileScrollPane);
+
+		// OPEN FILE BUTTON
+		// reference:http://www.codejava.net/java-se/swing/show-simple-open-file-dialog-using-jfilechooser
+		JButton openFileButton = new JButton("Open Java File");
+		openFileButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				selectedFile = openFile();
+				javaFileOpenedTextArea.append(selectedFile.getAbsolutePath());
+				insertCodeTextArea.setText("");
+				textPreEdit = parseFile(selectedFile);
+				insertCodeTextArea.append(textPreEdit);
+			}
+		});
+		openFileButton.setBounds(10, 11, 171, 23);
+		contentPane.add(openFileButton);
+
+		// SAVE BUTTON
+		JButton saveButton = new JButton("Save Selected File");
+		saveButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				textPostEdit = insertCodeTextArea.getText();
+				saveFile();
+			}
+		});
+		saveButton.setBounds(10, 113, 171, 23);
+		contentPane.add(saveButton);
+
+		JLabel lblSelectedFile = new JLabel("Selected File:");
+		lblSelectedFile.setBounds(10, 36, 171, 14);
+		contentPane.add(lblSelectedFile);
+	}
+
+	/***********************************************************************************************************************/
+
+	// OPEN FILE METHOD
+	public File openFile() {
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setCurrentDirectory(new File(System
+				.getProperty("user.home")));
+
+		fileChooser.setFileFilter(new FileNameExtensionFilter("Java Files",
+				"java"));
+
+		int result = fileChooser.showOpenDialog(this);
+		if (result == JFileChooser.APPROVE_OPTION) {
+			File selectedFile = fileChooser.getSelectedFile();
+			return selectedFile;
+		}
+		return null;
+	}
+
+	// FILE PARSER METHOD
+	public String parseFile(File file) {
+		String contents = "";
+		String line;
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			while ((line = reader.readLine()) != null) {
+				contents += line + "\n";
+			}
+			reader.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return contents;
+	}
+
+	// SAVE FILE METHOD
+	public void saveFile() {
+		int compareVal = textPreEdit.compareTo(textPostEdit);
+		if (compareVal != 0) {
+			BufferedWriter out;
+			try {
+				out = new BufferedWriter(new FileWriter(selectedFile));
+				out.write(textPostEdit);
+				out.close();
+				JOptionPane.showMessageDialog(null, "File Saved");
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+	}
+
+	// SEND SELECTED FILE METHOD
+	public void sendToServer() {
+		client = new Client();
+		boolean connected = client.connect();
+		client.sendData();
 	}
 }
