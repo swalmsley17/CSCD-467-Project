48ddd209095e6646bfe7248553f587e457649ad4
ewu-phillipsk <kphillips2010@eagles.ewu.edu>
2015-03-11 21:36:48 -0700

    Client connection
    Client pretty much good to go. Can open, save, save as, edit a file, and
    submit/connect to a server. Currently tested using HW45 server, works
    like a charm. Does not (as of yet) send the file over, but that is easy
    enough and only needs to send it to the server.

--- Client/clientWindow/Client.java
+++ Client/clientWindow/Client.java
@@ -1,16 +1,43 @@
 package clientWindow;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import javax.swing.JTextArea;
+
 public class Client {
+	
+	private ClientWindow clientWindow;
+	private BufferedReader in;
+	private PrintWriter out;
 	
 	public Client(){
 		
 	}
 	
-	public boolean connect(){
-		return false;
+	public void connectToServer(String serverAddress, String serverPort, File file, ClientWindow serverMessageField){
+		clientWindow = serverMessageField;
+		clientWindow.receiveServerMessage("Attempting connect: IP:" + serverAddress + ", Port: " + serverPort + "...");
+
+		try {
+			Socket socket = new Socket(serverAddress, Integer.parseInt(serverPort));
+			in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+			out = new PrintWriter(socket.getOutputStream(), true);
+			clientWindow.receiveServerMessage("Connected to server.");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			clientWindow.receiveServerMessage("Error occured when connecting to server, check the entered IP and port and try again later.");
+			e.printStackTrace();
+		}
 	}
 	
 	public void sendData(){
-		
+		//INCOMPLETE
 	}
 }
--- Client/clientWindow/ClientWindow.java
+++ Client/clientWindow/ClientWindow.java
@@ -48,6 +48,9 @@ public class ClientWindow extends JFrame {
 	private String textPostEdit = "";
 	private File selectedFile;
 	private Client client;
+	private JTextField serverIPTextField;
+	private JTextField serverPortTextField;
+	private JTextArea serverMessagesTextArea;
 
 	/**
 	 * Launch the application.
@@ -72,7 +75,7 @@ public class ClientWindow extends JFrame {
 	 */
 	public ClientWindow() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 620, 370);
+		setBounds(100, 100, 730, 440);
 
 		// MENU STUFF
 		JMenuBar menuBar = new JMenuBar();
@@ -80,14 +83,6 @@ public class ClientWindow extends JFrame {
 
 		JMenu mnFile = new JMenu("File");
 		menuBar.add(mnFile);
-
-		JMenuItem mntmExit = new JMenuItem("Exit");
-		mntmExit.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				System.exit(0);
-			}
-		});
-		mnFile.add(mntmExit);
 
 		JMenu mnHelp = new JMenu("Help");
 		menuBar.add(mnHelp);
@@ -106,20 +101,18 @@ public class ClientWindow extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		// INSERT CODE TEXT AREA + SCROLL PANE
-		JTextArea insertCodeTextArea = new JTextArea();
-		insertCodeTextArea.setTabSize(3);
-		insertCodeTextArea.setBounds(111, 41, 403, 259);
-		insertCodeTextArea.setBorder(BorderFactory
-				.createLineBorder(Color.black));
-		contentPane.add(insertCodeTextArea);
-
-		JScrollPane codeScrollPane = new JScrollPane(insertCodeTextArea);
-		codeScrollPane.setBounds(191, 40, 403, 259);
+		JScrollPane codeScrollPane = new JScrollPane();
+		codeScrollPane
+				.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		codeScrollPane.setBounds(301, 96, 403, 273);
 		contentPane.add(codeScrollPane);
 
+		JTextArea insertCodeTextArea = new JTextArea();
+		insertCodeTextArea.setTabSize(3);
+		codeScrollPane.setViewportView(insertCodeTextArea);
+
 		JLabel insertCodeHereLabel = new JLabel("Your Code Goes Here:");
-		insertCodeHereLabel.setBounds(191, 15, 132, 14);
+		insertCodeHereLabel.setBounds(301, 71, 132, 14);
 		contentPane.add(insertCodeHereLabel);
 
 		// SAVE AND SUBMIT BUTTON
@@ -130,38 +123,24 @@ public class ClientWindow extends JFrame {
 				sendToServer();
 			}
 		});
-		submitCodeButton.setBounds(10, 147, 171, 37);
+		submitCodeButton.setBounds(10, 74, 171, 37);
 		contentPane.add(submitCodeButton);
 
 		JLabel serverMessagesLabel = new JLabel("Server Messages:");
-		serverMessagesLabel.setBounds(10, 195, 171, 14);
+		serverMessagesLabel.setBounds(10, 199, 171, 14);
 		contentPane.add(serverMessagesLabel);
 
-		// SERVER MESSAGE TEXT AREA
-		JTextArea serverMessagesTextArea = new JTextArea();
-		serverMessagesTextArea.setTabSize(4);
-		serverMessagesTextArea.setEditable(false);
-		serverMessagesTextArea.setBounds(10, 220, 171, 79);
-		serverMessagesTextArea.setBorder(BorderFactory
-				.createLineBorder(Color.black));
-		contentPane.add(serverMessagesTextArea);
-
-		// OPENED JAVA FILE TEXT AREA + SCROLL PANE
-		JTextArea javaFileOpenedTextArea = new JTextArea();
-		javaFileOpenedTextArea.setBounds(10, 80, 169, 35);
-		contentPane.add(javaFileOpenedTextArea);
-		javaFileOpenedTextArea.setEditable(false);
-		javaFileOpenedTextArea.setBorder(BorderFactory
-				.createLineBorder(Color.black));
-
-		JScrollPane selectFileScrollPane = new JScrollPane(
-				javaFileOpenedTextArea);
+		JScrollPane selectFileScrollPane = new JScrollPane();
 		selectFileScrollPane
 				.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
 		selectFileScrollPane
 				.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
-		selectFileScrollPane.setBounds(10, 58, 171, 44);
+		selectFileScrollPane.setBounds(10, 144, 281, 44);
 		contentPane.add(selectFileScrollPane);
+
+		JTextArea javaFileOpenedTextArea = new JTextArea();
+		javaFileOpenedTextArea.setTabSize(3);
+		selectFileScrollPane.setViewportView(javaFileOpenedTextArea);
 
 		// OPEN FILE BUTTON
 		// reference:http://www.codejava.net/java-se/swing/show-simple-open-file-dialog-using-jfilechooser
@@ -175,6 +154,44 @@ public class ClientWindow extends JFrame {
 				insertCodeTextArea.append(textPreEdit);
 			}
 		});
+
+		JMenuItem mntmOpen = new JMenuItem("Open File...");
+		mntmOpen.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				selectedFile = openFile();
+				javaFileOpenedTextArea.append(selectedFile.getAbsolutePath());
+				insertCodeTextArea.setText("");
+				textPreEdit = parseFile(selectedFile);
+				insertCodeTextArea.append(textPreEdit);
+			}
+		});
+		mnFile.add(mntmOpen);
+
+		JMenuItem mntmSave = new JMenuItem("Save...");
+		mntmSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				textPostEdit = insertCodeTextArea.getText();
+				saveFile();
+			}
+		});
+		mnFile.add(mntmSave);
+
+		JMenuItem mntmSaveAs = new JMenuItem("Save as...");
+		mntmSaveAs.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				saveFileAs();
+			}
+		});
+		mnFile.add(mntmSaveAs);
+
+		JMenuItem mntmExit = new JMenuItem("Exit");
+		mntmExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
+		mnFile.add(mntmExit);
+
 		openFileButton.setBounds(10, 11, 171, 23);
 		contentPane.add(openFileButton);
 
@@ -186,18 +203,49 @@ public class ClientWindow extends JFrame {
 				saveFile();
 			}
 		});
-		saveButton.setBounds(10, 113, 171, 23);
+		saveButton.setBounds(10, 40, 171, 23);
 		contentPane.add(saveButton);
 
 		JLabel lblSelectedFile = new JLabel("Selected File:");
-		lblSelectedFile.setBounds(10, 36, 171, 14);
+		lblSelectedFile.setBounds(10, 122, 171, 14);
 		contentPane.add(lblSelectedFile);
+
+		JLabel lblServerIp = new JLabel("Server IP:");
+		lblServerIp.setBounds(301, 11, 61, 14);
+		contentPane.add(lblServerIp);
+
+		serverIPTextField = new JTextField();
+		serverIPTextField.setText("localhost");
+		serverIPTextField.setBounds(301, 36, 171, 20);
+		contentPane.add(serverIPTextField);
+		serverIPTextField.setColumns(10);
+
+		JLabel lblServerPort = new JLabel("Server Port:");
+		lblServerPort.setBounds(482, 11, 79, 14);
+		contentPane.add(lblServerPort);
+
+		serverPortTextField = new JTextField();
+		serverPortTextField.setText("9898");
+		serverPortTextField.setBounds(482, 36, 86, 20);
+		contentPane.add(serverPortTextField);
+		serverPortTextField.setColumns(10);
+
+		JScrollPane serverMessagesScrollPane = new JScrollPane();
+		serverMessagesScrollPane
+				.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		serverMessagesScrollPane.setBounds(10, 224, 281, 145);
+		contentPane.add(serverMessagesScrollPane);
+
+		serverMessagesTextArea = new JTextArea();
+		serverMessagesTextArea.setEditable(false);
+		serverMessagesTextArea.setTabSize(3);
+		serverMessagesScrollPane.setViewportView(serverMessagesTextArea);
 	}
 
 	/***********************************************************************************************************************/
 
 	// OPEN FILE METHOD
-	public File openFile() {
+	private File openFile() {
 		JFileChooser fileChooser = new JFileChooser();
 		fileChooser.setCurrentDirectory(new File(System
 				.getProperty("user.home")));
@@ -214,7 +262,7 @@ public class ClientWindow extends JFrame {
 	}
 
 	// FILE PARSER METHOD
-	public String parseFile(File file) {
+	private String parseFile(File file) {
 		String contents = "";
 		String line;
 
@@ -232,7 +280,7 @@ public class ClientWindow extends JFrame {
 	}
 
 	// SAVE FILE METHOD
-	public void saveFile() {
+	private void saveFile() {
 		int compareVal = textPreEdit.compareTo(textPostEdit);
 		if (compareVal != 0) {
 			BufferedWriter out;
@@ -248,10 +296,20 @@ public class ClientWindow extends JFrame {
 		}
 	}
 
+	private void saveFileAs() {
+		JFileChooser fileChooser = new JFileChooser();
+		int result = fileChooser.showSaveDialog(this);
+	}
+
 	// SEND SELECTED FILE METHOD
-	public void sendToServer() {
+	private void sendToServer() {
 		client = new Client();
-		boolean connected = client.connect();
-		client.sendData();
+		client.connectToServer(serverIPTextField.getText(),
+				serverPortTextField.getText(), selectedFile, this);
+	}
+
+	// SET SERVER MESSAGE BOX
+	public void receiveServerMessage(String text) {
+		serverMessagesTextArea.append(text + "\n");
 	}
 }
